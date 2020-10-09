0f2e8e718a316ad7d8474f8291f444b8a4aa5d09
nextdesusu <nextdesusu@gmail.com>
2020-10-10 00:36:04 +0400

    feat improvements to paddle_ball

--- src/games/paddle_ball/index.ts
+++ src/games/paddle_ball/index.ts
@@ -1,7 +1,5 @@
-import { gameArgs, strCb } from "../types";
-
-const KEY_LEFT = 37;
-const KEY_RIGHT = 39;
+import { gameArgs, strCb, KEYS } from "../types";
+import Connection from 'src/connection';
 
 interface Point {
   x: number;
@@ -23,35 +21,48 @@ interface Ball {
   pos: Point;
   vel: Point;
 }
-const enum ballDirections {
-  up = -1,
-  down = 1,
-}
-const rand = () => Math.random() > .5 ? ballDirections.up : ballDirections.down;
 
-const PADDLE_SIZE: Point = Object.freeze({ x: 50, y: 20 });
+const enum PADDLE_BALL_COLORS {
+  BALL = "red",
+  PADDLE = "blue",
+  FIELD = "gray",
+}
+
+const rand = () => Math.random() > .5 ? -1 : 1;
+
+const PADDLE_SIZE = Object.freeze({ width: 150, height: 20 });
 const PADDLE_SPEED = 2;
 
 const BALL_RADIUS = 20;
 const BALL_SPEED = 1;
 
+interface PaddleBallArgs {
+  connection: Connection;
+  ctx: CanvasRenderingContext2D;
+  boardSize: number;
+  isHost: boolean;
+  endCb: strCb;
+}
+
 export default class PaddleBall {
+  private connection: Connection;
   private selfPaddle: Paddle;
   private opPaddle: Paddle;
   private ball: Ball;
   private ctx: CanvasRenderingContext2D;
   private boardSize: number;
-  private endCb: strCb;
   private isHost: boolean;
-  constructor(ctx: CanvasRenderingContext2D, boardSize: number, endCb: strCb, isHost: boolean) {
+  private endCb: strCb;
+  constructor({ connection, ctx, boardSize, isHost, endCb }: PaddleBallArgs) {
+    this.connection = connection;
     this.ctx = ctx;
     this.boardSize = boardSize;
     this.endCb = endCb;
     this.isHost = isHost;
 
     const middle = Math.floor(boardSize / 2);
-    const paddleMiddle = middle - Math.floor(PADDLE_SIZE.x / 2);
-    const bottomY = boardSize - PADDLE_SIZE.y;
+    const paddleMiddle = middle - Math.floor(PADDLE_SIZE.width / 2);
+    const bottomY = boardSize - PADDLE_SIZE.height;
 
     this.ball = { pos: { x: middle, y: middle }, vel: { x: rand(), y: rand() } };
     const startP: Point = { x: paddleMiddle, y: 0 };
@@ -61,39 +72,37 @@ export default class PaddleBall {
       this.selfPaddle = { pos: startP, state: startS };
       this.opPaddle = { pos: endPos, state: startS };
     } else {
-      this.opPaddle = { pos: startP, state: startS };
       this.selfPaddle = { pos: endPos, state: startS };
+      this.opPaddle = { pos: startP, state: startS };
     }
   }
 
   static host({ connection, canvasNode, size, endCb }: gameArgs) {
-    console.log("host node:", canvasNode);
     const ctx = canvasNode.getContext("2d");
-    const game = new PaddleBall(ctx, size, endCb, true);
+    const game = new PaddleBall({
+      connection,
+      ctx,
+      boardSize: size,
+      endCb,
+      isHost: true
+    });
 
     document.addEventListener("keydown", (event: any) => {
-      console.log("join event:", event);
-      if (event.keyCode === KEY_LEFT) {
+      if (event.keyCode === KEYS.LEFT) {
         game.setSPaddleState(paddleState.goingLeft);
-      } else if (event.keyCode === KEY_RIGHT) {
+      } else if (event.keyCode === KEYS.RIGHT) {
         game.setSPaddleState(paddleState.goingRight);
       }
     });
     document.addEventListener("keyup", (event: any) => {
-      if (event.keyCode === KEY_LEFT || event.keyCode === KEY_RIGHT) {
+      if (event.keyCode === KEYS.LEFT || event.keyCode === KEYS.RIGHT) {
         game.selfPaddle.state = paddleState.stand;
       }
     })
 
     connection.setGameDataCb((gameData: any) => {
-      /*
-      const parsed = JSON.parse(gameData.data);
-      if (!parsed) return;
-      const [x, y] = parsed;
-      game.makeMove(x, y);
-      const data = JSON.stringify(game.state);
-      connection.sendGameData(data);
-      */
+      const pState = Number(gameData.data);
+      game.setOpPaddleState(pState);
     });
 
     game.start();
@@ -101,43 +110,52 @@ export default class PaddleBall {
 
   static join({ connection, canvasNode, size, endCb }: gameArgs) {
     const ctx = canvasNode.getContext("2d");
-    const game = new PaddleBall(ctx, size, endCb, false);
+    const game = new PaddleBall({
+      connection,
+      ctx,
+      boardSize: size,
+      endCb,
+      isHost: false
+    });
 
     document.addEventListener("keydown", (event: any) => {
-      console.log("join event:", event);
-      if (event.keyCode === KEY_LEFT) {
+      if (event.keyCode === KEYS.LEFT) {
         connection.sendGameData(String(paddleState.goingLeft));
-      } else if (event.keyCode === KEY_RIGHT) {
+      } else if (event.keyCode === KEYS.RIGHT) {
         connection.sendGameData(String(paddleState.goingRight));
       }
     });
     document.addEventListener("keyup", (event: any) => {
-      if (event.keyCode === KEY_LEFT || event.keyCode === KEY_RIGHT) {
+      if (event.keyCode === KEYS.LEFT || event.keyCode === KEYS.RIGHT) {
         connection.sendGameData(String(paddleState.stand));
       }
     })
 
     connection.setGameDataCb((gameData: any) => {
       //apply host response
-      game.applyGameData(gameData);
+      const data = JSON.parse(gameData.data);
+      if (data) {
+        game.applyGameData(gameData);
+      }
     });
 
     game.start();
   }
 
   applyGameData(gameData: any) {
+    console.log("applying data:", gameData)
     const { ballPos, hostP, clientP } = gameData;
     this.ball.pos = ballPos;
-    this.selfPaddle = clientP;
-    this.opPaddle = hostP;
+    this.selfPaddle.pos = clientP;
+    this.opPaddle.pos = hostP;
   }
 
   start() {
-    const ms100 = 100;
+    const ms10 = 10;
     let time = Date.now();
     const cb = () => {
       const now = Date.now();
-      if (now - time > ms100) {
+      if (now - time > ms10) {
         this.update();
         time = now;
       }
@@ -148,6 +166,7 @@ export default class PaddleBall {
 
   private drawBall() {
     const { x, y } = this.ball.pos;
+    this.ctx.beginPath();
     this.ctx.arc(x, y, BALL_RADIUS, 0, 2 * Math.PI);
     this.ctx.fill();
     this.ctx.closePath();
@@ -155,34 +174,47 @@ export default class PaddleBall {
 
   private drawPaddle(paddle: Paddle) {
     const { x, y } = paddle.pos;
-    const endX = x + PADDLE_SIZE.x;
-    const endY = y + PADDLE_SIZE.y;
-    //console.log("drawPaddle:", x, y, endX, endY);
-    this.ctx.fillRect(x, y, endX, endY);
+    this.ctx.fillRect(x, y, PADDLE_SIZE.width, PADDLE_SIZE.height);
   }
 
   setSPaddleState(ps: paddleState) {
     this.selfPaddle.state = ps;
   }
 
+  setOpPaddleState(ps: paddleState) {
+    this.opPaddle.state = ps;
+  }
+
   private movePaddle(paddle: Paddle) {
+    if (this.opPaddle.state !== 0) {
+      console.log("opPaddle.state:", this.opPaddle.state);
+    }
     if (paddle.state === paddleState.goingLeft) {
+      if (paddle === this.opPaddle) {
+        console.log("moving left opPaddle:", this.opPaddle);
+      }
       if (paddle.pos.x > 0.1) {
         paddle.pos.x -= PADDLE_SPEED;
       }
     } else if (paddle.state === paddleState.goingRight) {
-      if ((paddle.pos.x + PADDLE_SIZE.x) < this.boardSize) {
+      if (paddle === this.opPaddle) {
+        console.log("moving right opPaddle:", this.opPaddle);
+      }
+      if ((paddle.pos.x + PADDLE_SIZE.width) < this.boardSize) {
         paddle.pos.x += PADDLE_SPEED;
       }
     }
   }
 
   private moveBall() {
-    const { vel } = this.ball;
+    const { vel, pos } = this.ball;
     this.ball.pos.x += BALL_SPEED * vel.x;
     this.ball.pos.y += BALL_SPEED * vel.y;
 
-    if ((vel.x + BALL_RADIUS) > this.boardSize || (vel.x - BALL_RADIUS) < 0) {
+    if ((pos.y + BALL_RADIUS) > this.boardSize || (pos.y - BALL_RADIUS) < 0) {
+      vel.y *= -1;
+    }
+    if ((pos.x + BALL_RADIUS) > this.boardSize || (pos.x - BALL_RADIUS) < 0) {
       vel.x *= -1;
     }
   }
@@ -193,23 +225,34 @@ export default class PaddleBall {
     this.moveBall();
   }
 
+  private get currentGameData(): string {
+    return JSON.stringify({
+      ballPos: this.ball.pos,
+      hostP: this.selfPaddle.pos,
+      clientP: this.opPaddle.pos
+    });
+  }
+
 
   private update() {
     if (this.isHost) {
       this.moveAll();
+      if (this.connection.connectionEstablished) {
+        this.connection.sendGameData(this.currentGameData);
+      }
     }
 
     const s = this.boardSize;
-
-    this.ctx.fillStyle = "gray";
     this.ctx.clearRect(0, 0, s, s);
+
+    this.ctx.fillStyle = PADDLE_BALL_COLORS.FIELD;
     this.ctx.fillRect(0, 0, s, s);
 
-    this.ctx.fillStyle = "blue";
+    this.ctx.fillStyle = PADDLE_BALL_COLORS.PADDLE;
     this.drawPaddle(this.selfPaddle);
     this.drawPaddle(this.opPaddle);
 
-    this.ctx.fillStyle = "red";
+    this.ctx.fillStyle = PADDLE_BALL_COLORS.BALL;
     this.drawBall();
   }
 }
--- src/games/types.ts
+++ src/games/types.ts
@@ -2,6 +2,11 @@ import Connection from "../connection";
 
 export type strCb = (arg: string) => void;
 
+export const enum KEYS {
+  LEFT = 37,
+  RIGHT = 39
+}
+
 export interface gameArgs {
   connection: Connection;
   canvasNode: HTMLCanvasElement;
